8bc7d40d7b269d6ba1f89900034270aa440573e9
Fernando E. Zepeda Castellanos <fernando.zepeda@seguritech.com>
2023-04-25 09:31:27 -0600

    logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['warn', 'error', 'verbose'],
+    logger: ['warn', 'error', 'log', 'verbose'],
   });
   app.useGlobalPipes(
     new ValidationPipe({
